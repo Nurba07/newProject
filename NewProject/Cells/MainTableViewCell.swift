f22f4009eceb6470f4cd3e972c4d0e0144f93912
Nurba72 <nurbanauryzbay@gmail.com>
2022-03-15 13:16:28 +0600

    first commit

---
+++ NewProject/Cells/MainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.swift
+//  NewProject
+//
+//  Created by Nurba Seyilkhan on 15.03.2022.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ NewProject/Model/Partners.swift
@@ -0,0 +1,8 @@
+//
+//  Partners.swift
+//  NewProject
+//
+//  Created by Nurba Seyilkhan on 15.03.2022.
+//
+
+import Foundation
