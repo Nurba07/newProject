e5462445d3bfaf52e787faff87f1d068c9855818
Nurba72 <nurbanauryzbay@gmail.com>
2022-03-16 21:22:20 +0600

    testing present to another view after alert

--- NewProject/Controllers/AlertViewController.swift
+++ NewProject/Controllers/AlertViewController.swift
@@ -11,27 +11,27 @@ class AlertViewController: UIViewController {
     @IBOutlet weak var indicatorView: UIView!
     
     @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    let storyboard1 = UIStoryboard(name: "Main", bundle: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         indicatorView.layer.cornerRadius = 12
         self.indicator.startAnimating()
         let when = DispatchTime.now() + 2
         DispatchQueue.main.asyncAfter(deadline: when){
-            self.dismiss(animated: false, completion: nil)
+            self.dismiss(animated: false) {
+                weak var pvc = self.presentingViewController
+                self.dismiss(animated: true, completion: {
+                    let vc = TestViewController()
+                    pvc?.
+                })
+            }
+          
             
         }
         // Do any additional setup after loading the view.
     }
  
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
